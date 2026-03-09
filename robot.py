df3aa5f3363f91266cf52f86aa7139b3a9b8cbdb
NielsOostindie <niels@oostindie.com>
2026-03-09 10:59:36 +0100

    final code

--- robot.py
+++ robot.py
@@ -9,25 +9,15 @@ motor1_in1 = Pin(13, Pin.OUT)
 motor2_pwm = PWM(Pin(11), freq=1000)
 motor2_in1 = Pin(12, Pin.OUT)
 
-sensorL1 = ADC(Pin(2), atten=ADC.ATTN_11DB)
-sensorL2 = ADC(Pin(1), atten=ADC.ATTN_11DB)
-sensorM = ADC(Pin(7), atten=ADC.ATTN_11DB)
-sensorR2 = ADC(Pin(6), atten=ADC.ATTN_11DB)
-sensorR1 = ADC(Pin(5), atten=ADC.ATTN_11DB)
-
-
-def drive(left_speed, right_speed):
-    # Motor 1 = links
-    motor1_in1.value(1 if left_speed > 0 else 0)
-    motor1_pwm.duty_u16(int(abs(left_speed) * 65535))
-
-    # Motor 2 = rechts
-    motor2_in1.value(1 if right_speed > 0 else 0)
-    motor2_pwm.duty_u16(int(abs(right_speed) * 65535))
+ldr1 = ADC(Pin(2), atten=ADC.ATTN_11DB)
+ldr2 = ADC(Pin(1), atten=ADC.ATTN_11DB)
+ldr3 = ADC(Pin(7), atten=ADC.ATTN_11DB)
+ldr4 = ADC(Pin(6), atten=ADC.ATTN_11DB)
+ldr5 = ADC(Pin(5), atten=ADC.ATTN_11DB)
 
 def drive_forward(speed):
     motor1_pwm.duty_u16(int(65535 * speed))
-    motor2_pwm.duty_u16(int(65535 * speed))
+    motor2_pwm.duty_u16(int(65235 * speed))
     motor1_in1.value(0)  
     motor2_in1.value(1)
 
@@ -36,25 +26,37 @@ def stop():
     motor2_pwm.duty_u16(0)
 
 THRESHOLD = 30000
-BASE_SPEED = 0.4
-TURN_SPEED = 0.2
+BASE_SPEED = 0.35
+TURN_SPEED = 0.15
 
 last_direction = 0
 
 while True:
-    lijn_values = [
-        sensorL1.read_u16(),
-        sensorL2.read_u16(),
-        sensorM.read_u16(),
-        sensorR2.read_u16(),
-        sensorR1.read_u16()
+    ldr_values = [
+        ldr1.read_u16(),
+        ldr2.read_u16(),
+        ldr3.read_u16(),
+        ldr4.read_u16(),
+        ldr5.read_u16()
     ]
+    
+    print(ldr_values)
 
 
-    print(lijn_values)
+    left = ldr_values[0] < THRESHOLD
+    left_mid = ldr_values[1] < THRESHOLD
+    center = ldr_values[2] < THRESHOLD
+    right_mid = ldr_values[3] < THRESHOLD
+    right = ldr_values[4] < THRESHOLD
 
-    if lijn_values[2] < THRESHOLD:
+    if center:
         drive_forward(BASE_SPEED)
-    else: 
-        stop()
-    print(lijn_values)
+    elif right_mid or right:
+        motor1_in1.value(1)
+    elif left_mid or left:
+        motor2_in1.value(0)
+
+    else:
+        motor2_in1.value(0)
+
+    # time.sleep(0.02)
